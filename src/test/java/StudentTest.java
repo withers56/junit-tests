6a289535b6953e48d70e90796c6374cec687887d
withers56 <william.k.withers@gmail.com>
2022-03-22 13:40:51 -0500

    junit testing first exercise

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -8,34 +8,27 @@ import static org.junit.Assert.assertEquals;
 
 public class StudentTest {
 
-    public String name;
-    private long id;
-    private ArrayList<Integer> grades;
-
-    @Before
-    public void setup() {
-        name = "will";
-        id = 6;
-        grades = new ArrayList<>();
-        grades.add(99);
-        grades.add(90);
-    }
-
     @Test
     public void testIfGettingId() {
-        assertEquals(6, getId());
+        Student s1 = new Student("will", 6);
+        assertEquals(6, s1.getId());
     }
 
     @Test
     public void testIfGettingName(){
-        assertEquals("will", getName());
+        Student s1 = new Student("will", 6);
+        assertEquals("will", s1.getName());
     }
 
     @Test
     public void testIfAverageIsCorrect() {
         double average = (double) (99 + 90) / 2;
 
-        assertEquals(average, getGradeAverage(), 2);
+        Student s1 = new Student("will", 6);
+        s1.addGrade(99);
+        s1.addGrade(90);
+
+        assertEquals(average, s1.getGradeAverage(), 2);
     }
 
     @Test
@@ -44,49 +37,12 @@ public class StudentTest {
         testGrades.add(99);
         testGrades.add(90);
 
+        Student s1 = new Student("will", 6);
+        s1.addGrade(99);
+        s1.addGrade(90);
+
         for (int i = 0; i < testGrades.size(); i++) {
-            assertEquals(testGrades.get(i), grades.get(i));
+            assertEquals(testGrades.get(i), s1.getGrades().get(i));
         }
-    }
-
-
-
-
-    public void addGrade(int grade) {
-        getGrades().add(grade);
-    }
-
-    public double getGradeAverage() {
-        long total = 0;
-        for (Integer grade : grades) {
-            total += grade;
-        }
-        return (double)(total / grades.size());
-    }
-
-    //accessor
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public ArrayList<Integer> getGrades() {
-        return grades;
-    }
-
-    public void setGrades(ArrayList<Integer> grades) {
-        this.grades = grades;
     }
 }
